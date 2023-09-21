60adba132e914c2f612f4e157f5b1ccd461b0e01
jedrpn <104227716+jedrpn@users.noreply.github.com>
2023-09-21 18:40:55 -0500

    Update CombateJugador.cs

--- Assets/CombateJugador.cs
+++ Assets/CombateJugador.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CombateJugador : MonoBehaviour
 {
@@ -8,11 +9,20 @@ public class CombateJugador : MonoBehaviour
     [SerializeField] float maximoVida;
     [SerializeField] private BarraDeVida barraDeVida;
     float xInicial, yInicial;
+    [SerializeField] private int vidaMaxima;
+
+
+    private Animator animator;
+
+    public event EventHandler MuerteJugador;
+
+    
 
 
     // Start is called before the first frame update
     private void Start()
     {
+        animator = GetComponent<Animator>();
         vida = maximoVida;
         barraDeVida.InicializarBarraDeVida(vida);
         xInicial = transform.position.x;
@@ -21,10 +31,22 @@ public class CombateJugador : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void TomarDaño(float damage)
+    public void TomarDaño(int daño)
     {
         vida -= damage;
         barraDeVida.CambiarVidaActual(vida);
+        if (vida > 0)
+        {
+            animator.SetTrigger("Golpe");
+        }
+        else
+        {
+            animator.SetTrigger("Dead");
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Jugador"), LayerMask.NameToLayer("Enemigo"),
+                true);
+
+        }
+
         if (vida <= 0)
         {
             Destroy(gameObject);
@@ -33,6 +55,10 @@ public class CombateJugador : MonoBehaviour
     public void Recolocar()
     {
         transform.position = new Vector3(xInicial, yInicial, 0);
+    }
+    public void MuerteJugadorEvento()
+    {
+        MuerteJugador?.Invoke(this, EventArgs.Empty);
     }
 
 }
